1d1274b059db9406111a3fe952abc40cd7fdf166
ghislain01 <ghislainatsin01@gmail.com>
2025-02-26 17:54:53 +0000

    Premier commit du projet Spring boot

---
+++ src/main/java/com/example/monprojetspringboot/MonprojetspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.monprojetspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonprojetspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonprojetspringbootApplication.class, args);
+	}
+
+}
